cec5e7b446c80bd27f60a4541d681aa7a8911ea6
Ahmed Hassan <0ahmed.sial@gmail.com>
2026-04-14 07:42:51 +0500

    initial project setup

---
+++ src/main/java/com/ahmedhassan/speakly/SpeaklyApplication.java
@@ -0,0 +1,14 @@
+package com.ahmedhassan.speakly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeaklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeaklyApplication.class, args);
+	}
+
+}
+jldjfalsdfjaldfjals fj
