e77bd1363b8571cf9b2f8cfbac6ff9dbbcc0056e
Ahmed Hassan <0ahmed.sial@gmail.com>
2026-04-16 21:55:06 +0500

    add user mapper that maps to entity and domain model

---
+++ src/main/java/com/ahmedhassan/speakly/modules/user/internal/application/UserMapper.java
@@ -0,0 +1,33 @@
+package com.ahmedhassan.speakly.modules.user.internal.application;
+
+import com.ahmedhassan.speakly.modules.user.internal.domain.Email;
+import com.ahmedhassan.speakly.modules.user.internal.domain.User;
+import com.ahmedhassan.speakly.modules.user.internal.domain.UserId;
+import com.ahmedhassan.speakly.modules.user.internal.infrastructure.persistence.UserEntity;
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public User toDomain(@NonNull UserEntity userEntity) {
+        return User.reconstitute(
+                UserId.of(userEntity.getUserId()),
+                userEntity.getFullName(),
+                Email.of(userEntity.getEmail()),
+                userEntity.getPasswordHash(),
+                userEntity.getIsActive(),
+                userEntity.getLastSeen()
+        );
+    }
+
+    public UserEntity toEntity(@NonNull User domain) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(domain.getUserId().value());
+        userEntity.setFullName(domain.getFullName());
+        userEntity.setEmail(domain.getEmail().value());
+        userEntity.setPasswordHash(domain.getPasswordHash());
+        userEntity.setIsActive(domain.isActive());
+        userEntity.setLastSeen(domain.getLastSeen());
+        return userEntity;
+    }
+}
