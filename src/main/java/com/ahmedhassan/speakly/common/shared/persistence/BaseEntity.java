3ba86266dc593c7075a1eedc9bc2c818e28b29c5
Ahmed Hassan <0ahmed.sial@gmail.com>
2026-04-16 21:47:52 +0500

    add base entity for easy auditing

---
+++ src/main/java/com/ahmedhassan/speakly/common/shared/persistence/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.ahmedhassan.speakly.common.shared.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private Instant updatedAt;
+}
