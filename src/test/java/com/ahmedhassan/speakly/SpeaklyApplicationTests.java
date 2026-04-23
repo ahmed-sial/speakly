0e53a1623ad376f00f6420ebfd2b9ac61cfe856b
Ahmed Hassan <0ahmed.sial@gmail.com>
2026-04-23 20:59:53 +0500

    initial project setup

---
+++ src/test/java/com/ahmedhassan/speakly/SpeaklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmedhassan.speakly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeaklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
